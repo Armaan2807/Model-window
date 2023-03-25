d50cebce65b80b35cfa2cc5d9f72308ff7247104
armaan <emiratestrader@gmail.com>
2023-03-25 13:53:58 +0530

    completed this example

---
+++ script.js
@@ -0,0 +1,9 @@
+function showModal (){
+    document.querySelector(".modal").classList.remove("hidden");
+    document.querySelector(".overlay").classList.remove("hidden");
+}
+function hideModal (){
+    document.querySelector(".modal").classList.add("hidden");
+    document.querySelector(".overlay").classList.add("hidden");
+
+}
